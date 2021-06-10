52decbf670f5da3529c716b63c9a2df88d6ab146
DmitryWS <48471255+DmitryWS@users.noreply.github.com>
2021-06-10 18:44:46 +0300

    Add files via upload

---
+++ Memento.java
@@ -0,0 +1,21 @@
+
+public class Memento {
+	private String state;
+	private int playerNumber;
+	
+	public String getState() {
+		return state;
+	}
+	
+	public void setState(String state) {
+		this.state = state;
+	}
+	
+	public int getPlayerNumber() {
+		return playerNumber;
+	}
+	
+	public void setPlayerNumber(int playerNumber) {
+		this.playerNumber = playerNumber;
+	}
+}
